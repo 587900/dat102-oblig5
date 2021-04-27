77c5e5893e7820f6f1889abe5d007e7b45f674c2
587900 <587900@stud.hvl.no>
2021-04-27 13:27:57 +0200

    3d

---
+++ dat102-oblig5/src/no/hvl/dat102/Student.java
@@ -0,0 +1,12 @@
+package no.hvl.dat102;
+
+public class Student {
+	private int snr;
+	private String namn;
+	
+	public Student(int snr, String namn) {
+		this.snr = snr;
+		this.namn = namn;
+	}
+
+}
